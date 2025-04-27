59b5d777f2e53161c36429bd08346557fdd58ad5
yamahaoo <som.29.off@gmail.com>
2025-04-26 19:02:21 -0600

    Update bgsi.lua

--- bgsi.lua
+++ bgsi.lua
@@ -7,6 +7,3 @@ task.spawn(function()
     (loadstring or load)(game:HttpGet("https://raw.githubusercontent.com/MateoDev2024/MoonX/refs/heads/main/Loader.lua"))()
 end)
 
-task.spawn(function()
-    loadstring(game:HttpGet("https://raw.githubusercontent.com/AhmadV99/Speed-Hub-X/main/Speed%20Hub%20X.lua", true))()
-end)
